fd03487c4eed41e2fd87fe081a3db9a3767dfb80
sergey-mario <sergey-mario@yandex.ru>
2019-10-07 21:31:42 +0300

    Task 1

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Pen pen = new Pen(1000, 1.0);
+        pen.write("word");
+        pen.getColor();
+        pen.doSomethingElse();
+
+    }
+}
